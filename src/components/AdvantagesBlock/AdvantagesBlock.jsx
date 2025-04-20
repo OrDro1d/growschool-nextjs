cad4a51aa526a758d14ffef0731363a646291806
Osman Lyumanov <osmanlyumanov2005@gmail.com>
2025-04-20 23:28:41 +0300

    Был полностью перерисован блок Advantages. Теперь страница выглядит более целостно при изменении масштабов окна. Прочие изменения в стилях

--- src/components/AdvantagesBlock/AdvantagesBlock.jsx
+++ src/components/AdvantagesBlock/AdvantagesBlock.jsx
@@ -9,26 +9,59 @@ export default function AdvantagesBlock() {
 		<div className={styles["advantages-background"]}>
 			<h2 className={poiretOne.className}>Преимущества нашей платформы</h2>
 			<div className={styles["advantages-block"]}>
-				<div className={`${styles["quality"]} ${styles["advantage"]}`}>
-					<h3 className={poiretOne.className}>Качество</h3>
-					<p>
-						Хоть курсы и создаются независимыми командами, они все проверены и
-						соответствуют нашим стандартам качества
-					</p>
+				<div
+					className={`${styles["advantage-block"]} ${styles["quality-block"]}`}
+				>
+					<Image
+						className={styles["advantage-img"]}
+						src="/images/icons/25.png"
+						alt=""
+						width={256}
+						height={256}
+					></Image>
+					<div className={styles["advantage"]}>
+						<h3 className={poiretOne.className}>Качество</h3>
+						<p>
+							Хоть курсы и создаются независимыми командами, они все проверены и
+							соответствуют нашим стандартам качества
+						</p>
+					</div>
 				</div>
-				<div className={`${styles["security"]} ${styles["advantage"]}`}>
-					<h3 className={poiretOne.className}>Безопасность</h3>
-					<p>
-						Мы заботимся о вашей безопасности и конфиденциальности. Мы собираем
-						только необходимые для работы сервиса данные
-					</p>
+				<div
+					className={`${styles["advantage-block"]} ${styles["security-block"]}`}
+				>
+					<div className={styles["advantage"]}>
+						<h3 className={poiretOne.className}>Безопасность</h3>
+						<p>
+							Мы заботимся о вашей безопасности и конфиденциальности. Мы
+							собираем только необходимые для работы сервиса данные
+						</p>
+					</div>
+					<Image
+						className={styles["advantage-img"]}
+						src="/images/icons/18.png"
+						alt=""
+						width={256}
+						height={256}
+					></Image>
 				</div>
-				<div className={`${styles["support"]} ${styles["advantage"]}`}>
-					<h3 className={poiretOne.className}>Поддержка</h3>
-					<p>
-						Мы всегда рядом! Напишите, если у вас возникли трудности и мы
-						поможем их преодолеть
-					</p>
+				<div
+					className={`${styles["advantage-block"]} ${styles["support-block"]}`}
+				>
+					<Image
+						className={styles["advantage-img"]}
+						src="/images/icons/15.png"
+						alt=""
+						width={256}
+						height={256}
+					></Image>
+					<div className={styles["advantage"]}>
+						<h3 className={poiretOne.className}>Поддержка</h3>
+						<p>
+							Мы всегда рядом! Напишите, если у вас возникли трудности и мы
+							поможем их преодолеть
+						</p>
+					</div>
 				</div>
 			</div>
 			<Image
--- src/components/HereForYouBlock/HereForYouBlock.jsx
+++ src/components/HereForYouBlock/HereForYouBlock.jsx
@@ -1,4 +1,7 @@
 import Image from "next/image";
+
+import { poiretOne } from "@/app/layout";
+
 import styles from "./HereForYouBlock.module.css";
 
 export default function HereForYouBlock() {
@@ -11,11 +14,12 @@ export default function HereForYouBlock() {
 					width={1004}
 					height={635}
 				></Image>
-				<h2>Платформа - созданная для вас</h2>
+				<h1 className={poiretOne.className}>Платформа - созданная для вас</h1>
 				<p>
 					Мы знаем не понаслышке насколько это сложно научиться чему-то новому,
-					особенно, если у вас пока нет ничего конкретного, кроме желания. А
-					потому поддерживаем вас <br></br>на каждом этапе обучения
+					<br></br>
+					особенно, если у вас пока нет ничего конкретного, кроме желания.
+					<br></br> А потому поддерживаем вас на каждом этапе обучения
 				</p>
 			</div>
 		</div>
